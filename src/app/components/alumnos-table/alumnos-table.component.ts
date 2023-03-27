da432f254fb2c35d3be682f1294b8b52f34a80c3
Leo <leo.virgolini@gmail.com>
2023-03-27 13:08:00 -0300

    actualizado

--- src/app/components/alumnos-table/alumnos-table.component.ts
+++ src/app/components/alumnos-table/alumnos-table.component.ts
@@ -13,23 +13,23 @@ export class AlumnosTableComponent implements OnInit {
 
   constructor() {
     this.cargando = true;
-    
-    this.alumnos = [
-      new Alumno(1, 'Juan', 'Perez', new Date('1991/07/22')),
-      new Alumno(2, 'Tomás', 'Roncero', new Date('1985/08/11')),
-      new Alumno(3, 'Carlos', 'Jimenez', new Date('1984/09/24')),
-      new Alumno(4, 'Mateo', 'Juarez', new Date('1990/10/10')),
-      new Alumno(5, 'Micaela', 'Ramirez', new Date('1991/11/05')),
-      new Alumno(6, 'Camila', 'Herrera', new Date('1992/12/07')),
-      new Alumno(7, 'Sofía', 'Narvaez', new Date('1986/01/19')),
-      new Alumno(8, 'Fernando', 'Iglesias', new Date('1987/05/08')),
-      new Alumno(9, 'Romina', 'Sanchez', new Date('1984/04/17')),
-      new Alumno(10, 'Pedro', 'Gonzalez', new Date('1988/01/25'))
-    ];
+    this.alumnos = [];
   }
 
   ngOnInit(): void {
     setTimeout(() => {
+      this.alumnos = [
+        new Alumno(1, 'Juan', 'Perez', new Date('1991/07/22')),
+        new Alumno(2, 'Tomás', 'Roncero', new Date('1985/08/11')),
+        new Alumno(3, 'Carlos', 'Jimenez', new Date('1984/09/24')),
+        new Alumno(4, 'Mateo', 'Juarez', new Date('1990/10/10')),
+        new Alumno(5, 'Micaela', 'Ramirez', new Date('1991/11/05')),
+        new Alumno(6, 'Camila', 'Herrera', new Date('1992/12/07')),
+        new Alumno(7, 'Sofía', 'Narvaez', new Date('1986/01/19')),
+        new Alumno(8, 'Fernando', 'Iglesias', new Date('1987/05/08')),
+        new Alumno(9, 'Romina', 'Sanchez', new Date('1984/04/17')),
+        new Alumno(10, 'Pedro', 'Gonzalez', new Date('1988/01/25'))
+      ];
       this.cargando = false;
     }, 1000);
   }
